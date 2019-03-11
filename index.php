7e4293f1908a00f9b2e9ca4e28ac7bf6c1ab0120
yukta12 <yuktapeswani12@gmail.com>
2019-03-11 16:07:38 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,380 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Learn zilla</title>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <!-- ElegantFonts CSS -->
+    <link rel="stylesheet" href="css/elegant-fonts.css">
+
+    <!-- themify-icons CSS -->
+    <link rel="stylesheet" href="css/themify-icons.css">
+
+    <!-- Swiper CSS -->
+    <link rel="stylesheet" href="css/swiper.min.css">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="hero-content">
+        <header class="site-header">
+            <div class="top-header-bar">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 col-lg-6 d-none d-md-flex flex-wrap justify-content-center justify-content-lg-start mb-3 mb-lg-0">
+                            <div class="header-bar-email d-flex align-items-center">
+                                <i class="fa fa-envelope"></i><a href="#">learnzilla.12@gmail.com</a>
+                            </div><!-- .header-bar-email -->
+
+                            <div class="header-bar-text lg-flex align-items-center">
+                                <p><i class="fa fa-phone"></i>001-1234-88888 </p>
+                            </div><!-- .header-bar-text -->
+                        </div><!-- .col -->
+
+                        <div class="col-12 col-lg-6 d-flex flex-wrap justify-content-center justify-content-lg-end align-items-center">
+                            
+
+                            <div class="header-bar-menu">
+                                <ul class="flex justify-content-center align-items-center py-2 pt-md-0">
+                                    <li><a href="#">Register </a></li>
+                                    <li><a href="#"> Login</a></li>
+                                </ul>
+                            </div><!-- .header-bar-menu -->
+                        </div><!-- .col -->
+                    </div><!-- .row -->
+                </div><!-- .container-fluid -->
+            </div><!-- .top-header-bar -->
+
+            <div class="nav-bar">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-9 col-lg-3">
+                            <div class="site-branding">
+                                <h1 class="site-title"><a href="index.php" rel="home">Learn<span>Zilla</span></a></h1>
+                            </div><!-- .site-branding -->
+                        </div><!-- .col -->
+
+                        <div class="col-3 col-lg-9 flex justify-content-end align-content-center">
+                            <nav class="site-navigation flex justify-content-end align-items-center">
+                                <ul class="flex flex-column flex-lg-row justify-content-lg-end align-content-center">
+                                    <li class="current-menu-item"><a href="index.php">Home</a></li>
+                                    <li><a href="#about">About</a></li>
+                                    <li><a href="#">Courses</a></li>
+                    
+                                    <li><a href="#contact">Contact</a></li>
+                                </ul>
+
+                                <div class="hamburger-menu d-lg-none">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div><!-- .hamburger-menu -->
+
+                                
+                            </nav><!-- .site-navigation -->
+                        </div><!-- .col -->
+                    </div><!-- .row -->
+                </div><!-- .container -->
+            </div><!-- .nav-bar -->
+        </header><!-- .site-header -->
+
+        <div class="hero-content-overlay">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="hero-content-wrap flex flex-column justify-content-center align-items-start">
+                            <header class="entry-header">
+                                <h4>Get started with online courses</h4>
+                                <h1>best online<br/>Learning system</h1>
+                            </header><!-- .entry-header -->
+
+                            <div class="entry-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, repudiandae veniam porro autem, corporis nulla obcaecati perspiciatis magni itaque veritatis voluptate id sed praesentium repellendus, vero placeat facilis aliquam molestiae.</p>
+                            </div><!-- .entry-content -->
+
+                            <footer class="entry-footer read-more">
+                                <a href="#">Contact us</a>
+                            </footer><!-- .entry-footer -->
+                        </div><!-- .hero-content-wrap -->
+                    </div><!-- .col -->
+                </div><!-- .row -->
+            </div><!-- .container -->
+        </div><!-- .hero-content-hero-content-overlay -->
+    </div><!-- .hero-content -->
+
+
+
+
+    <section class="about-section" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-6 align-content-lg-stretch">
+                    <header class="heading">
+                        <h2 class="entry-title">About learnZilla</h2>
+
+                        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>
+                    </header><!-- .heading -->
+
+                    <div class="entry-content learn-zilla-stats">
+                        <div class="stats-wrap flex flex-wrap justify-content-lg-between">
+                            <div class="stats-count">
+                                50<span>M+</span>
+                                <p>STUDENTS LEARNING</p>
+                            </div><!-- .stats-count -->
+
+                            <div class="stats-count">
+                                30<span>K+</span>
+                                <p>ACTIVE COURSES</p>
+                            </div><!-- .stats-count -->
+
+                            <div class="stats-count">
+                                340<span>+</span>
+                                <p>INSTRUCTORS</p>
+                            </div><!-- .stats-count -->
+
+                            <div class="stats-count">
+                                20<span>+</span>
+                                <p>Country Reached</p>
+                            </div><!-- .stats-count -->
+                        </div><!-- .stats-wrap -->
+                    </div><!-- .learn-zilla-stats -->
+                </div><!-- .col -->
+
+                <div class="col-12 col-lg-6 flex align-content-center mt-5 mt-lg-0">
+                    <div class="learn-zilla-video position-relative">
+                        <div class="video-play-btn position-absolute">
+                            <img src="images/video-icon.png" alt="Video Play">
+                        </div><!-- .video-play-btn -->
+
+                        <img src="images/video-screenshot.png" alt="">
+                    </div><!-- .learn-zilla-video -->
+                </div><!-- .col -->
+            </div><!-- .row -->
+        </div><!-- .container -->
+    </section><!-- .about-section -->
+
+    <section class="testimonial-section">
+        <!-- Swiper -->
+        <div class="swiper-container testimonial-slider">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 col-lg-6 order-2 order-lg-1 flex align-items-center mt-5 mt-lg-0">
+                                <figure class="user-avatar">
+                                    <img src="images/user-1.jpg" alt="">
+                                </figure><!-- .user-avatar -->
+                            </div><!-- .col -->
+
+                            <div class="col-12 col-lg-6 order-1 order-lg-2 content-wrap h-100">
+                                <div class="entry-content">
+                                    <p>Together as teachers, students and universities we can help make this education available for everyone.</p>
+                                </div><!-- .entry-content -->
+
+                                <div class="entry-footer">
+                                    <h3 class="testimonial-user">Russell Stephens - <span>University in UK</span></h3>
+                                </div><!-- .entry-footer -->
+                            </div><!-- .col -->
+                        </div><!-- .row -->
+                    </div><!-- .container -->
+                </div><!-- .swiper-slide -->
+
+                <div class="swiper-slide">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 col-lg-6 order-2 order-lg-1 flex align-items-center mt-5 mt-lg-0">
+                                <figure class="user-avatar">
+                                    <img src="images/user-2.jpg" alt="">
+                                </figure><!-- .user-avatar -->
+                            </div><!-- .col -->
+
+                            <div class="col-12 col-lg-6 order-1 order-lg-2 content-wrap h-100">
+                                <div class="entry-content">
+                                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                </div><!-- .entry-content -->
+
+                                <div class="entry-footer">
+                                    <h3 class="testimonial-user">Robert Stephens - <span>University in Oxford</span></h3>
+                                </div><!-- .entry-footer -->
+                            </div><!-- .col -->
+                        </div><!-- .row -->
+                    </div><!-- .container -->
+                </div><!-- .swiper-slide -->
+
+                <div class="swiper-slide">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 col-lg-6 flex order-2 order-lg-1 align-items-center mt-5 mt-lg-0">
+                                <figure class="user-avatar">
+                                    <img src="images/user-3.jpg" alt="">
+                                </figure><!-- .user-avatar -->
+                            </div><!-- .col -->
+
+                            <div class="col-12 col-lg-6 order-1 order-lg-2 content-wrap h-100">
+                                <div class="entry-content">
+                                    <p>Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.</p>
+                                </div><!-- .entry-content -->
+
+                                <div class="entry-footer">
+                                    <h3 class="testimonial-user">James Stephens - <span>University in Cambridge</span></h3>
+                                </div><!-- .entry-footer -->
+                            </div><!-- .col -->
+                        </div><!-- .row -->
+                    </div><!-- .container -->
+                </div><!-- .swiper-slide -->
+            </div><!-- .swiper-wrapper -->
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-lg-6 mt-5 mt-lg-0">
+                        <div class="swiper-pagination position-relative flex justify-content-center align-items-center"></div>
+                    </div><!-- .col -->
+                </div><!-- .row -->
+            </div><!-- .container -->
+        </div><!-- .testimonial-slider -->
+    </section><!-- .testimonial-section -->
+
+    
+
+    <section class="home-gallery">
+        <div class="gallery-wrap flex flex-wrap">
+            <div class="gallery-grid gallery-grid1x1">
+                <a href="#"><img src="images/a.jpg" alt=""></a>
+            </div><!-- .gallery-grid -->
+
+            <div class="gallery-grid gallery-grid1x1">
+                <a href="#"><img src="images/b.jpg" alt=""></a>
+            </div><!-- .gallery-grid -->
+
+            <div class="gallery-grid gallery-grid2x2">
+                <a href="#"><img src="images/c.jpg" alt=""></a>
+            </div><!-- .gallery-grid -->
+
+            <div class="gallery-grid gallery-grid1x1">
+                <a href="#"><img src="images/d.jpg" alt=""></a>
+            </div><!-- .gallery-grid -->
+
+            <div class="gallery-grid gallery-grid1x1">
+                <a href="#"><img src="images/e.jpg" alt=""></a>
+            </div><!-- .gallery-grid -->
+
+            <div class="gallery-grid gallery-grid2x1">
+                <a href="#"><img src="images/g.jpg" alt=""></a>
+            </div><!-- .gallery-grid -->
+
+            <div class="gallery-grid gallery-grid2x1">
+                <a href="#"><img src="images/h.jpg" alt=""></a>
+            </div><!-- .gallery-grid -->
+
+            <div class="gallery-grid gallery-grid1x1">
+                <a href="#"><img src="images/i.jpg" alt=""></a>
+            </div><!-- .gallery-grid -->
+
+            <div class="gallery-grid gallery-grid2x2 ">
+                <a href="#"><img src="images/j.jpg" alt=""></a>
+            </div><!-- .gallery-grid -->
+
+            <div class="gallery-grid gallery-grid1x1">
+                <a href="#"><img src="images/k.jpg" alt=""></a>
+            </div><!-- .gallery-grid -->
+
+            <div class="gallery-grid gallery-grid1x1">
+                <a href="#"><img src="images/l.jpg" alt=""></a>
+            </div><!-- .gallery-grid -->
+
+            <div class="gallery-grid gallery-grid2x1">
+                <a href="#"><img src="images/m.jpg" alt=""></a>
+            </div><!-- .gallery-grid -->
+
+            <div class="gallery-grid gallery-grid3x1">
+                <a href="#"><img src="images/n.jpg" alt=""></a>
+            </div><!-- .gallery-grid -->
+
+            <div class="gallery-grid gallery-grid1x1">
+                <a href="#"><img src="images/o.jpg" alt=""></a>
+            </div><!-- .gallery-grid -->
+        </div><!-- .gallery-wrap -->
+    </section><!-- .home-gallery -->
+
+
+
+    <footer class="site-footer" id="contact">
+        <div class="footer-widgets">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="foot-about">
+                            
+                            <h1 class="site-title"><a href="index.php" rel="home"><span style="color: #000;">Learn</span><span>Zilla</span></a></h1>
+                            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia dese mollit anim id est laborum. </p>
+
+                            
+                        </div><!-- .foot-about -->
+                    </div><!-- .col -->
+
+                    <div class="col-12 col-md-6 col-lg-3 mt-5 mt-md-0">
+                        <div class="foot-contact">
+                            <h2>Contact Us</h2>
+
+                            <ul>
+                                <li>Email: rose.learnZilla@hotmail.com</li>
+                                <li>Phone: (+88) 111 555 666</li>
+                                <li>Address: 40 Baria Sreet 133/2 NewYork City, US</li>
+                            </ul>
+                        </div><!-- .foot-contact -->
+                    </div><!-- .col -->
+
+                   
+
+                    <div class="col-12 col-md-6 col-lg-3 mt-5 mt-lg-0">
+                        <div class="follow-us">
+                            <h2>Follow Us</h2>
+
+                            <ul class="follow-us flex flex-wrap align-items-center">
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            </ul>
+                        </div><!-- .quick-links -->
+                    </div><!-- .col -->
+                </div><!-- .row -->
+            </div><!-- .container -->
+        </div><!-- .footer-widgets -->
+
+        <div class="footer-bar">
+            <div class="container">
+                <div class="row flex-wrap justify-content-center justify-content-lg-between align-items-center">
+                    <div class="col-12 col-lg-6">
+                        <div class="download-apps flex flex-wrap justify-content-center justify-content-lg-start align-items-center">
+                            <a href="#"><img src="images/app-store.png" alt=""></a>
+                            <a href="#"><img src="images/play-store.png" alt=""></a>
+                        </div><!-- .download-apps -->
+
+                    </div>
+
+                    
+                </div><!-- .row -->
+            </div><!-- .container -->
+        </div><!-- .footer-bar -->
+    </footer><!-- .site-footer -->
+
+<script type='text/javascript' src='js/jquery.js'></script>
+<script type='text/javascript' src='js/swiper.min.js'></script>
+<script type='text/javascript' src='js/masonry.pkgd.min.js'></script>
+<script type='text/javascript' src='js/jquery.collapsible.min.js'></script>
+<script type='text/javascript' src='js/custom.js'></script>
+
+</body>
+</html>
